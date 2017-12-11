b03b143726bbe8c57dd3b39f3611e680df2575a0
Jennifer Dobak <jdobak1@gmail.com>
2017-12-11 15:42:59 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
 def position_taken? (board, index)
   if board[index] == "   " || board[index] == " " || board[index] == nil
     return false
-  else board[index] == "X" || "O"
+  else board[index] == "X" || board[index] == "O"
     return true
   end
 end
